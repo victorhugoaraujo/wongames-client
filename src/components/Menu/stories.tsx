c572fba8b1e44a1446bccaba3038d72dff4f635c
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-10-19 22:06:07 -0300

    Add MenuFull styles

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -1,9 +1,16 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Menu from '.'
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Menu from '.';
 
 export default {
   title: 'Menu',
-  component: Menu
-} as Meta
+  component: Menu,
+} as Meta;
 
-export const Default: Story = () => <Menu />
+export const Default: Story = () => <Menu />;
+
+Default.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark',
+  },
+};
