0ec3ca9cb0af4e475527ce2648003a85782cbafc
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-10-19 22:23:30 -0300

    Add MenuFull unit tests

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,7 +8,11 @@ import Logo from 'components/Logo';
 import * as S from './styles';
 import { useState } from 'react';
 
-const Menu = () => {
+export type MenuProps = {
+  username?: string;
+};
+
+const Menu = ({ username }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <S.Wrapper>
@@ -32,17 +36,26 @@ const Menu = () => {
         <S.MenuNav>
           <S.MenuLink href="#">Home</S.MenuLink>
           <S.MenuLink href="#">Explore</S.MenuLink>
+
+          {!!username && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
         </S.MenuNav>
 
-        <S.RegisterBox>
-          <Button fullWidth size="large">
-            Log in now
-          </Button>
-          <span>or</span>
-          <S.CreateAccount href="#" title="Sign Up">
-            Sign Up
-          </S.CreateAccount>
-        </S.RegisterBox>
+        {!username && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="Sign Up">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
       </S.MenuFull>
     </S.Wrapper>
   );
