96e87c9bdfa18f77d05c323fcb6a11b831064bf4
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-10-22 22:42:38 -0300

    Add component Highlight, Hightlight with float image

---
+++ src/components/Highlight/test.tsx
@@ -0,0 +1,47 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import Highlight from '.';
+
+const props = {
+  title: 'Heading 1',
+  subtitle: 'Heading 2',
+  buttonLabel: 'Buy now',
+  backgroundImage: '/img/red-dead-img.jpg',
+  buttonLink: '/rdr2',
+};
+
+describe('<Highlight />', () => {
+  it('should render headings and button', () => {
+    renderWithTheme(<Highlight {...props} />);
+
+    expect(
+      screen.getByRole('heading', { name: /Heading 1/i })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Heading 2/i })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument();
+
+    // expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render background image', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />);
+
+    expect(container.firstChild).toHaveStyle({
+      backgroundImage: `url(${props.backgroundImage})`,
+    });
+  });
+
+  it('should render float image', () => {
+    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />);
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      '/float-image.png'
+    );
+  });
+});
